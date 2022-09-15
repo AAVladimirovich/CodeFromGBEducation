9a61dbcb813ff0eb2538181d44bcc19d6e3bb1e7
Alexey <a.v.alyokhin@mail.ru>
2022-09-15 17:24:14 +0300

    Добавил файл HWS03_05.py и файл FunctionHolder.py - добавил функцию negafibonacci - ряд фибоначи + работает с отрицательными числами, в папке HomeWorkSeminar03 - Задача - Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов. Пример: - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]

--- Python/HomeWorkSeminar03/FunctionHolder.py
+++ Python/HomeWorkSeminar03/FunctionHolder.py
@@ -30,3 +30,35 @@ def to_basenum_system_conversion(arg_number, basenum):
     elif arg_number == 0:
         return 0
     return arg_number % basenum + 10 * to_basenum_system_conversion(arg_number // basenum, basenum)
+
+
+def negafibonacci(arg_number):
+    if arg_number >= 0:
+        if arg_number == 1 or arg_number == 2:
+            return 1
+        elif arg_number == 0:
+            return 0
+        else:
+            return negafibonacci(arg_number - 1) + negafibonacci(arg_number - 2)
+    elif arg_number < 0:
+        if arg_number == -1 or arg_number == -2:
+            return -1
+        else:
+            return negafibonacci(arg_number + 1) + negafibonacci(arg_number + 2)
+
+
+# void Fibonacci(int in_num)
+# {
+#     int f1 = 0;
+#     int f2 = 1;
+#     int sum = 0;
+#     Console.Write($"{f1} {f2}");
+#
+#     for (int i = 1; i <= in_num; i++)
+#     {
+#         sum = f1 + f2;
+#         f1 = f2;
+#         f2 = sum;
+#         Console.WriteLine($"F({i}) = {sum}");
+#     }
+# }
