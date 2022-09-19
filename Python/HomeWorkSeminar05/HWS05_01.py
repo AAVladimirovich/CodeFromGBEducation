6399162e172df9b8b00a53d778cac9128588673a
Alexey <a.v.alyokhin@mail.ru>
2022-09-19 19:36:13 +0300

    Добавил файл HWS05_01.py - Задача: В файле находится N натуральных чисел, записанных через пробел. Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
    файл utils_v3.py добавил файл версии 3, для дальшего ведения
    
    всё хранится в папке HomeWorkSeminar05

---
+++ Python/HomeWorkSeminar05/HWS05_01.py
@@ -0,0 +1,40 @@
+# В файле находится N натуральных чисел, записанных через пробел. Среди чисел не
+# хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
+
+import utils_v3 as hh  # hh - homework helper
+import random
+
+print(
+    'PyCharm\n'
+    'В файле находится N натуральных чисел, записанных через пробел. Среди чисел не\n'
+    'хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число\n'
+    )
+len_of_list = int(input('введите длину списка '))
+task_string = ''
+missed_number = random.randrange(len_of_list)
+
+for i in range(len_of_list):
+    if i is not missed_number:
+        task_string += str(i) + ' '
+
+hh.write_to_file("HWS05_01.txt", task_string, 'w')
+work_list = hh.read_from_file("HWS05_01.txt", True)   # получаем строку с файла и присваиваем стринговой переменной
+work_list = work_list.split()   # разбиваем полученую строку по пробелам и переводим её в лист
+print(f'Лист полученный с файла {work_list}')
+current_number = 0
+next_number = 0
+for i in range(len_of_list):
+    if i == 0:  # так как лист начинается с 0 индекса и это значение может быть пропущено, то приравниваем в ручную на проверку
+        current_number = 0
+        if current_number != int(work_list[i]):
+            print(f'Отсутствующая цифра = {current_number}')
+            exit()
+    current_number = int(work_list[i])
+    try:
+        next_number = int(work_list[i+1])  # так как лист может оканчиваться на последний элемент который отличный от общего списка, то проверяем на существование элемента
+    except IndexError:
+        print(f'Отсутствующая цифра = {current_number + 1}')
+        exit()
+    if next_number - 1 != current_number:
+        print(f'Отсутствующая цифра = {next_number - 1}')
+        exit()
---
+++ Python/HomeWorkSeminar05/utils_v3.py
@@ -0,0 +1,192 @@
+import random
+import re
+
+
+# функция определения числа, чётного или нет
+def is_it_odd(arg_number):
+    if arg_number % 2 == 1:
+        return True
+    else:
+        return False
+
+
+# функция возврата дробной части
+def get_fractional_part(arg_number):
+    whole_part = arg_number // 1
+    fractional_part = round(arg_number - whole_part, 5)
+    if fractional_part == 0:
+        return
+    else:
+        return fractional_part
+
+# функция преобразования в двоичный код
+def to_decimal(arg_number):
+    if arg_number == 1:
+        return 1
+    elif arg_number == 0:
+        return 0
+    return arg_number % 2 + 10 * to_decimal(arg_number // 2)
+
+# функция преобразования в код заданный basenum - ограничение 2 до 9 наверное.
+def to_basenum_system_conversion(arg_number, basenum):
+    if arg_number == 1:
+        return 1
+    elif arg_number == 0:
+        return 0
+    return arg_number % basenum + 10 * to_basenum_system_conversion(arg_number // basenum, basenum)
+
+# фибоначчи ряд, возвращающий от негативного числа
+def negafibonacci(arg_number):
+    if arg_number >= 0:
+        if arg_number == 1 or arg_number == 2:
+            return 1
+        elif arg_number == 0:
+            return 0
+        else:
+            return negafibonacci(arg_number - 1) + negafibonacci(arg_number - 2)
+    elif arg_number < 0:
+        if arg_number == -1 or arg_number == -2:
+            return -1
+        else:
+            return negafibonacci(arg_number + 1) + negafibonacci(arg_number + 2)
+
+
+# определение наименьшего общего делителя???
+def nod(a, b):
+    if b == 0:
+        return a
+    else:
+        return nod(b, a % b)
+
+
+# функция определения наименьшего общего кратного
+def nok(a, b):
+    return int(a * b) / nod(a, b)
+
+
+# функция решета Эратосфена
+def sieve_list(arg_int):
+    sieve = set(range(2, arg_int+1))
+    prime_list = []
+    while sieve:
+        prime = min(sieve)
+        prime_list.append(prime)
+        #print(sieve)
+        #print(prime)
+        sieve -= set(range(prime, arg_int + 1, prime))
+        #print(sieve)
+    #print(prime_list)
+    return prime_list
+
+
+# функция возвращения уникальных значений из списка
+def get_unique_numbers(numbers):
+    unique = []
+    for number in numbers:
+        if number not in unique:
+            unique.append(number)
+    return unique
+
+
+# проверка ввода
+def key_check(arg_string, arg_wich_check='int', arg_len=15):
+    if any([i > 'z' or i < 'a' for i in arg_string]) and arg_wich_check == 'string':
+        print(f'Error: Contains illegal characters, not {arg_wich_check}')
+        return False
+    if any([i > '9' or i < '0' for i in arg_string]) and arg_wich_check == 'int':
+        print(f'Error: Contains illegal characters, not {arg_wich_check}')
+        return False
+    elif len(arg_string) > arg_len:
+        print("Very long string")
+    return True
+
+
+# функция работы с файлом, когда-нибудь переделать правильнее, может распределить отдельно на запись и на чтение
+def write_to_file(arg_filename, arg_data, arg_write_func: str = 'a'):
+    # открываем файл для изменения 'w' - значит перезапись - изменяем содержимое с чистого листа
+    # открываем файл для изменения 'a' - значит добавление - добовляем содержимое в файл
+    # открываем файл для изменения 'r' - значит чтение содержимого файла
+    if arg_write_func == 'w' or arg_write_func == 'a':
+        with open(arg_filename, arg_write_func) as data:
+            data.write(arg_data)
+            data.write('\n')
+        data.close()
+
+
+# функция чтения из файла
+def read_from_file(arg_filename, return_required: bool = False):
+    string_to_return = ''
+    data = open(arg_filename)  # открываем файл для чтения
+    for line in data:  # перебираем файл по строкам и печатаем
+        print(f'значение строки внутри файла {arg_filename} : {line}')
+        string_to_return += line
+    data.close()
+    if return_required:
+        return string_to_return
+
+
+# функция задачи рандомного многочлена, дичь лютая конечно
+def polynomial(arg_koef, arg_max_rnd: int = 100):
+    a = int(random.randint(1, arg_max_rnd))
+    b = int(random.randint(1, arg_max_rnd))
+    c = int(random.randint(1, arg_max_rnd))
+
+    if a != 0:
+        first = (str(a) + "x^" + str(arg_koef) + " + ")
+    else:
+        first = (str())
+
+    if b != 0:
+        second = (str(b) + "x" + " + ")
+    else:
+        second = (str())
+
+    if c != 0:
+        third = (str(c) + " = 0")
+    else:
+        third = (str())
+
+    return first + second + third
+
+
+# посмотрел в интернете позволяет очистить строку от разных символов, заменить на " " указано до определения Word.strip()
+def clean(word):
+    return re.sub(r"[`x^+=!?.:;,'\"()-]", " ", word.strip())
+
+
+# функция инверсии бита
+def inverse_bit(arg_bit):
+    return arg_bit ^ True
+
+# list comprehansion
+# [print(f' множитель числа {n} = {item} *') for item in answer_list if item > 5]
+
+# мусорка. когда-нибудь разобрать
+# void Fibonacci(int in_num)
+# {
+#     int f1 = 0;
+#     int f2 = 1;
+#     int sum = 0;
+#     Console.Write($"{f1} {f2}");
+#
+#     for (int i = 1; i <= in_num; i++)
+#     {
+#         sum = f1 + f2;
+#         f1 = f2;
+#         f2 = sum;
+#         Console.WriteLine($"F({i}) = {sum}");
+#     }
+# }
+
+# #функция определения простых множителей числа
+# def pmc(arg_number):
+#     pmc_list = []
+#     while arg_number != 1:
+#         prime_list = sieve_list(arg_number)
+#         first_min_prime = False
+#         for current_prime in prime_list:
+#             if arg_number % current_prime == 0 and first_min_prime == False:
+#                 first_min_prime = True
+#                 pmc_list.append(current_prime)
+#                 pmc(arg_number / current_prime)
+#                 return pmc_list.append(current_prime)
