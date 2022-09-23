b99d2429cc84db763f4e4ce3c7ae58a0ea5742cc
Alexey <a.v.alyokhin@mail.ru>
2022-09-23 18:47:41 +0300

    Добавил файл HWS04_01.py - Задача: Вычислить число c заданной точностью d Пример:при d = 0.001, π = 3.141 диапазон 10-1 <= d <= 10-10. изменил cnhjrb c 10-15, 17.

--- Python/HomeWorkSeminar04/HWS04_01.py
+++ Python/HomeWorkSeminar04/HWS04_01.py
@@ -7,12 +7,13 @@ import math
 print('*PyCharm*. Вычислить число c заданной точностью d')
 print('*PyCharm*. Пример:при d = 0.001, π = 3.141 диапазон 10-1 <= d <= 10-10')
 print('*' * 16, 'Решение', '*' * 16)
-print('задайте d от 1 до 10 знаков после запятой = ')
-d = int(input())   # стоило бы запилить проверку на число
+print('задайте d от 1 до 10 знаков после запятой (прим. 0.001) = ')
+d = input()
+d = d.split('.')
+first_part = d[0]
+second_part = d[1]
+d = len(second_part)
 if 10 >= d >= 1:
-    # pi = float('{:.4f}'.format(math.pi))
-    # print(pi)
-    # print('%.4f' % math.pi)
     print(round(math.pi, d))
 else:
     print('d установлен 3')
